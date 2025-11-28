b1767912878e3cd99484925495ffefe3f70a05bd
P20000 <pranavdwviedi45645@gmail.com>
2025-11-28 22:55:10 +0530

    removed unnecesary placements

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -608,7 +608,8 @@ function App() {
                 
                 {/* Desktop Navigation Rail */}
                 {!isMobile && (
-                    <Box sx={{ mb: 4, display: 'flex', alignItems: 'center', justifyContent: 'center', height: 40, width: '100%' }}>
+                    <Box sx={{ width: 80, height: '100%', bgcolor: 'background.paper', borderRight: 1, borderColor: 'divider', display: 'flex', flexDirection: 'column', alignItems: 'center', py: 2 }}>
+                        <Box sx={{ mb: 4, display: 'flex', alignItems: 'center', justifyContent: 'center', height: 40, width: '100%' }}>
                             {/* NEW: Wrapper Box to constrain the SVG size */}
                             <Box sx={{ width: 32, height: 32, color: 'primary.main', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                                 <CustomLogo 
@@ -619,6 +620,7 @@ function App() {
                                         height: '100%'
                                     }} 
                                 />
+                            </Box>
                         </Box>
                         
                         <Fab color="secondary" size="medium" sx={{ mb: 4, boxShadow: 0 }} onClick={() => setIsModalOpen(true)}>
